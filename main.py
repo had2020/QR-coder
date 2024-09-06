afa9ef25d4da55f7ad8010a6bf9533bdf63fa9fb
Hadrian Lazic <hadrian.lazic@gmail.com>
2024-09-06 10:40:43 -0700

    fixed internal server errors when sending qr code in pywebview

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 import os
 import qrcode
 import datetime
-import webview
+import webview # import as pywebview
+import random
 
 deletion_due = None
 
@@ -33,7 +34,8 @@ def generate_qrcode(url):
     stringged_url = str(url)
     image_folder = "qr-code-pngs/"
     #image_name = stringged_url + time_obj to long names but sometimes works
-    image_path = image_folder + time_obj + ".png"
+    #image_path = image_folder + time_obj + ".png"
+    image_path = image_folder + str(random.randint(1, 100)) + ".png"
 
     # create image for code
     img = qr.make_image(fill='black', back_color='white')
@@ -53,7 +55,8 @@ def process():
   url = request.form['url']
   image_path = str(generate_qrcode(url))
   print(image_path)
-  return send_from_directory('qr-code-pngs/', image_path)#, delete_file(image_path) TODO
+  return send_from_directory('qr-code-pngs', os.path.basename(image_path)), delete_file(image_path)
+  #return send_from_directory('qr-code-pngs/', image_path)#, delete_file(image_path) TODO FAILS
 
 if __name__ == '__main__':
   #app.run(debug=True, host='0.0.0.0', port=5001) # change to ip and port for non-debug
